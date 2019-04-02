25e79b292fc7be12c42433bd879e9ba26b9f2c33
ihsin <luharihsin@gmail.com>
2019-04-02 21:38:56 +0530

    Add two numbers

---
+++ practice/Add.py
@@ -0,0 +1,13 @@
+#Solution to https://www.codechef.com/problems/FLOW001
+t= input()
+t= int(t)
+
+
+for x in range(t,0,-1):
+    a,b=0,0
+    a,b=  input().split()
+    a=int(a)
+    b=int(b)
+    print(a+b)
+    
+    
